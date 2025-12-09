2c879c8bb208745ba053a3d136bbb82abe30352b
Tanmay Gupta <tanmaygupta1706@gmail.com>
2025-12-09 23:30:29 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 app.use(express.json());
